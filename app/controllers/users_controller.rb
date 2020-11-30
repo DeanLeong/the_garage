6242ba6c7f7e9c09166df19dadf2a4d5acc3885a
Dean <dean.g.leong@gmail.com>
2020-11-30 17:40:12 -0500

    working on auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,6 @@ before_action :set_user, only: [:show, :update, :destroy]
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :age)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
