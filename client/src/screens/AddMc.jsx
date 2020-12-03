57f58b681aec023433a2729dc604fb9174e788ca
Dean <dean.g.leong@gmail.com>
2020-12-03 15:18:50 -0500

    add mc works on refresh

--- client/src/screens/AddMc.jsx
+++ client/src/screens/AddMc.jsx
@@ -5,24 +5,28 @@ import {Link, Redirect, useParams, useHistory} from 'react-router-dom'
 
 function AddMc(props) {
   const [motorcycle, setMotorcycle] = useState({
-    name: "",
-    img_url: ""
+    name: '',
+    img_url: '',
   })
   const [isCreated, setCreated] = useState(false)
   console.log(props)
-  const { motorcycle_id, user_id } = useParams()
+  const {id, user_id } = useParams()
+
+  console.log(id)
 
   const handleChange = (event) => {
     const { name, value } = event.target
     setMotorcycle({
       ...motorcycle,
-      [name]: value
+      [name]: value,
     })
   }
 
+  console.log(motorcycle)
+
  const  handleSubmit = async (event) => {
     event.preventDefault()
-    const created = await postMotorcycle(user_id, motorcycle_id)
+    const created = await postMotorcycle(motorcycle)
     setCreated({ created })
   }
   if (isCreated) {
@@ -38,12 +42,21 @@ function AddMc(props) {
           className="add-mc-name"
           placeholder="Motorcycle Name"
           value={motorcycle.name}
-          name="content"
+          name="name"
           required
+          autoFocus
           onChange={handleChange}
         />
         <input
           className="add-mc-img"
+          placeholder="Add an image URL for your Motorcycle"
+          value={motorcycle.img_url}
+          name="img_url"
+          autoFocus
+          required
+          onChange={handleChange}
+        />
+        <button type="submit" className="basic-button">Submit</button>
       </form>
     </div>
   );
